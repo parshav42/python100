7301dc9b27f8b8d63509e8d86cd4b2963d18fd41
parshav42 <singhrohan98192@gmail.com>
2026-07-27 22:36:57 +0530

    pythone code add

--- python.py
+++ python.py
@@ -1,28 +1,28 @@
-import os
-from pathlib import Path
-# user = str(input("please give loction of folder"))
-user = '/home/parshav/Documents'
-
-path = Path(user)
-pdf =[]
-filepy=[]
-jpeg =[]
-
-for i in path.rglob("*.pdf"):
-    if i.is_file():
-        pdf.append(i.name)
-
-
-for i in path.rglob("*.jpeg"):
-    for j in path.rglob("*.jpg"):
-        if j.is_file():
-            filepy.append(j.name)
-        elif i.is_file():
-            jpeg.append(i.name)
-
-
-
-
+# import os
+# from pathlib import Path
+# # user = str(input("please give loction of folder"))
+# user = '/home/parshav/Documents'
+#
+# path = Path(user)
+# pdf =[]
+# filepy=[]
+# jpeg =[]
+#
+# for i in path.rglob("*.pdf"):
+#     if i.is_file():
+#         pdf.append(i.name)
+#
+#
+# for i in path.rglob("*.jpeg"):
+#     for j in path.rglob("*.jpg"):
+#         if j.is_file():
+#             filepy.append(j.name)
+#         elif i.is_file():
+#             jpeg.append(i.name)
+#
+#
+#
+#
 
 
 
